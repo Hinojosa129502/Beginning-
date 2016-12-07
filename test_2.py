392c77be3d61f59a8533eb4c2925ce771d2c87c0
Jose Hinojosa <josehinojosa@Joses-MacBook-Pro.local>
2016-12-06 20:34:09 -0600

    Commit Test_2_1

--- test_2.py
+++ test_2.py
@@ -1,7 +1,12 @@
-from string import maketrans
-intab = "aeiou"
-outtab = "12345"
-transtab = maketrans(intab, outtab)
+# from string import maketrans
+# intab = "aeiou"
+# outtab = "12345"
+# transtab = maketrans(intab, outtab)
+#
+# str = "Testing, Testing; One, Two, Three"
+# print str.maketrans(transtab)
 
-str = "Testing, Testing; One, Two, Three"
-print str.maketrans(transtab)
+import string
+trans = string.maketrans('ae', 'bx')
+text = 'abcdef'
+print text.translate(trans)
