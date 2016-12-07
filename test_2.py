aaf7a07adf62fcdcb4b42fb6db0c33b42f3e8324
Jose Hinojosa <josehinojosa@Joses-MacBook-Pro.local>
2016-12-06 21:01:33 -0600

    commit Test_2_2

--- test_2.py
+++ test_2.py
@@ -1,12 +1,12 @@
-# from string import maketrans
-# intab = "aeiou"
-# outtab = "12345"
-# transtab = maketrans(intab, outtab)
-#
-# str = "Testing, Testing; One, Two, Three"
-# print str.maketrans(transtab)
-
 import string
 trans = string.maketrans('ae', 'bx')
 text = 'abcdef'
 print text.translate(trans)
+
+trans_2 = string.maketrans(string.ascii_lowercase, string.ascii_lowercase[2:] + string.ascii_lowercase[:2])
+test_string = 'fcjjm fmu ypc wms'
+print test_string.translate(trans_2)
+
+trans_3 = string.maketrans(string.digits, string.digits[3:] + string.digits[:3])
+test_string_2 = '137263'
+print test_string_2.translate(trans_3)
