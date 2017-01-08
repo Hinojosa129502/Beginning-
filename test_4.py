4a2e2a545c702fe35c239faf9b58b6915386aa83
Jose Hinojosa <josehinojosa@Joses-MBP.attlocal.net>
2017-01-08 15:28:44 -0600

    test_4_2

--- test_4.py
+++ test_4.py
@@ -2,11 +2,12 @@
 # The results should be printed in a comma-separated sequence on a single line.
 # Suppose the following input is supplied to the program:
 
-input_number = input('Input a number: ')
+input_number = int(input('Input a number: '))
+
 x = input_number #4
 add_list = []
 y = input_number
 while x > 2:
     y = y * (x-1) #12
     x -= 1
-print y
+print(y)
